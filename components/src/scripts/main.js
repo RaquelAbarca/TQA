1bb869f3fe2127375cb4dca8cda8ea94913f9fc6
andres <andres.villalobos@datagrove.com>
2023-06-13 11:25:35 -0600

    nav bar shows on mobile

--- components/src/scripts/main.js
+++ components/src/scripts/main.js
@@ -1,22 +1,22 @@
 //hide menu when display is mobile
-const navToggle = document.querySelector('#menu-toggle')
-const navi = document.querySelector('nav')
-const navIcon = document.querySelectorAll('.nav-icon')
-const hamburger = document.querySelector('#menu-toggle')
+// const navToggle = document.querySelector('#menu-toggle')
+// const navi = document.querySelector('nav')
+// const navIcon = document.querySelectorAll('.nav-icon')
+// const hamburger = document.querySelector('#menu-toggle')
 
-navToggle.addEventListener(
-    'click', () => {
-        navi.classList.toggle('open');
-        navIcon.forEach(icon => {
-            icon.classList.toggle('hidden')
-        })
-    })
-window.addEventListener('resize', () => {
-    if(document.body.clientWidth > 720){
-    navi.classList.remove('open');
-    navIcon.forEach(icon, () => {
-        icon.classList.add('hidden')
-    });
-    hamburger.classList.remove('hidden')
-    }
-});    
+// navToggle.addEventListener(
+//     'click', () => {
+//         navi.classList.toggle('open');
+//         navIcon.forEach(icon => {
+//             icon.classList.toggle('hidden')
+//         })
+//     })
+// window.addEventListener('resize', () => {
+//     if(document.body.clientWidth > 720){
+//     navi.classList.remove('open');
+//     navIcon.forEach(icon, () => {
+//         icon.classList.add('hidden')
+//     });
+//     hamburger.classList.remove('hidden')
+//     }
+// });    
